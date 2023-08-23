35a9614fedbc98750168845bd73d05e3f4710946
Yash Kumar <yashyk9891@gmail.com>
2023-08-23 17:38:32 +0530

    social links added

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { FaBars, faBars, FaTimes } from "react-icons/fa";
 import { useState } from "react";
-// import { Link } from "react-scroll";
 
 const NavBar = () => {
   const [nav, setNav] = useState(false);
@@ -34,12 +33,13 @@ const NavBar = () => {
   ];
   return (
     <div className="flex justify-between items-center w-full h-20 px-4 text-white bg-black fixed">
+      
+      {/* signature */}
       <div>
         <h1 className="text-5xl font-signature mt-2">Yash</h1>
       </div>
 
       {/* links */}
-
       <ul className="hidden md:flex">
         {links.map(({ id, link }) => (
           <li
@@ -52,7 +52,7 @@ const NavBar = () => {
         ))}
       </ul>
 
-      {/* icons */}
+      {/* Hamburger icon*/}
       <div
         onClick={() => setNav(!nav)}
         className="cursor-pointer pr-4 z-10 text-gray-500 md:hidden"
@@ -60,6 +60,7 @@ const NavBar = () => {
         {nav ? <FaTimes size={30} /> : <FaBars size={30} />}
       </div>
 
+      {/* navbar in vertical when hamburger icon clicked */}
       {nav&&(
         <ul className="flex flex-col justify-center items-center absolute top-0 left-0 w-full h-screen bg-gradient-to-b from-black to-gray-800 text-gray-500">
         {links.map(({ id, link }) => (
