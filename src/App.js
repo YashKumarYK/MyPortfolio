6586519a77b212f5b143757a1a79cfbde5d79927
Yash Kumar <yashyk9891@gmail.com>
2023-08-23 16:21:26 +0530

    Home.js added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import NavBar from "./components/NavBar";
-
+import Home from "./components/Home";
 function App() {
   return (
     <div className="App">
       <NavBar/>
+      <Home/>
     </div>
   );
 }
